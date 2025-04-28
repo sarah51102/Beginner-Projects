64e8df290d86e0a72b2c4b04c7ce3706c272b7ac
Sarah <ssolano5@mail.valenciacollege.edu>
2025-04-28 15:29:16 -0400

    Create GitHubCoinGame.py

---
+++ GitHubCoinGame.py
@@ -0,0 +1,8 @@
+import random
+num = random.randint (0, 1)
+    #generates a random number between 0 and 1
+if num > 0.5 :
+    print ("Heads")
+else:
+    print ("Tails")
+# This program simulates a coin flip using the random module in Python.
