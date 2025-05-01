fc4775f645fad99e6e1c691b96e727f0ca101084
Sarah <ssolano5@mail.valenciacollege.edu>
2025-05-01 12:32:10 -0400

    CreateMadlibsGame.py

---
+++ MadlibsGame.py
@@ -0,0 +1,14 @@
+# Madlibs Game
+# This is a simple Madlibs game where the user is prompted to enter words to fill in the blanks in a story.
+# The story is then displayed with the user's words inserted into it.
+
+adjective1 = input("Enter an adjective description): ")
+noun1 = input("Enter a noun: ")
+adjective2 = input("Enter another adjective: ")
+verb1 = input("Enter a verb ending with 'ing': ")
+adjective3 = input("Enter an adjective (description): ")
+
+print(f"Today I went to a {adjective1} party.")
+print(f"At a nearby table, I saw {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
